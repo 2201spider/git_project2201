02861bf9299fb72e89c7e20f57f2ff5e1e1fa1cd
nitro-alt <2584676863@qq.com>
2024-10-13 21:05:06 +0800

    Add files via upload

---
+++ test05.py
@@ -0,0 +1,12 @@
+import requests
+
+headers = {
+    "Accept": "text/html,app lication/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "Accept-Encoding": "gzip, deflate",
+    "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36 Edg/129.0.0.0'
+
+          }
+r = requests.get('http://httpbin.org/get', headers=headers)
+
+print(r.text)
